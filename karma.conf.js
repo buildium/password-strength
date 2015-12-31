1b632a0b64388e1ff960c8cc980299f400e0d39b
Calvin McEachron <calvin.mceachron@buildium.com>
2015-12-31 09:24:23 -0500

    Added builder pattern to close #3
    Also added the idea of _extensions_ with the passwordmeter.com settings
    as the default extension. Other extensions can have different rules and
    rates per rule, allowing the user to chose the appropriate extension for
    their password scheme or build one of their own.

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,8 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            '*.test.js'
+            '*.test.js',
+            'lib/**/*.test.js'
         ],
 
 
@@ -27,7 +28,8 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            '*.test.js': ['browserify']
+            '*.test.js': ['browserify'],
+            'lib/**/*.test.js': ['browserify']
         },
 
 
